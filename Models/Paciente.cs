15f27a42fcc4778f9c75511fd9eb9feca5f15100
Gabriel Luiz <gabriellnsilva2004@gmail.com>
2024-10-06 15:22:44 -0300

    Envio de arquivos

---
+++ Models/Paciente.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Hospital.Models
+{
+    [Table("Pacientes")]
+    public class Paciente
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+    }
+}
